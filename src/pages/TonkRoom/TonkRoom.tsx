46e66f5e7b55b79a04a4afee5c75e0b6ebaa6e6e
Jordan Goyena-Segarra <goyenasegarrajordan@gmail.com>
2024-03-01 14:37:26 -0500

    removing babylonJS from repo

--- src/pages/TonkRoom/TonkRoom.tsx
+++ src/pages/TonkRoom/TonkRoom.tsx
@@ -1,56 +1,59 @@
-import React, { FC, useContext } from 'react'
-import { Vector3 } from '@babylonjs/core/Maths/math.vector'
-import { Engine, Scene } from 'react-babylonjs'
+import React, { useState, useEffect, useContext } from 'react';
+import { Engine, Scene, AdvancedDynamicTexture, Rectangle, TextBlock, StackPanel, Button } from 'react-babylonjs';
 import { ColyseusContext } from '../../components/ColyseusProvider';
 
-/**
- *
- * TODO: camera autoAttaching?
- */
-const WithPointerDragBehavior: FC = () => {
-  const { room } = useContext(ColyseusContext)
+type Action = 'Move' | 'Fire' | 'Reload' | 'Rotate Turret';
+
+function TonkRoom() {
+  const { room } = useContext(ColyseusContext);
+  const [currentPlayer, setCurrentPlayer] = useState<string | null>(null);
+  const [actions, setActions] = useState<Action[]>([]);
+
+  useEffect(() => {
+    if (room) {
+      room.onMessage('turn', (message) => {
+        setCurrentPlayer(message.playerId);
+        setActions([]); // Reset actions at the start of each turn
+      });
+    }
+  }, [room]);
+
+  const handleActionClick = (action: Action) => {
+    if(!room)return;
+    if (actions.length < 3) {
+      setActions([...actions, action]);
+    }
+
+    if (actions.length === 2) {
+      room.send('actions', actions.concat(action));
+    }
+  };
+
+  if(!room)return;
+  
   return (
-    <>
-      <sphere
-        name="sphere1"
-        diameter={2}
-        segments={16}
-        position={new Vector3(0, 1, 0)}
-      >
-        <pointerDragBehavior
-          dragAxis={new Vector3(1, 0, 0)}
-          useObjectOrientationForDragging={true}
-          onDragStartObservable={(_: any) => console.log(room)}
-          onDragObservable={(_: any) => console.log('drag')}
-          onDragEndObservable={(_: any) => console.log('dragEnd')}
-        />
-      </sphere>
-      <ground name="ground1" width={6} height={6} subdivisions={2} />
-    </>
-  )
+    <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
+      <Scene>
+        
+          <plane name="gui-plane" size={5}>
+            <advancedDynamicTexture name="adTexture" createForParentMesh hasAlpha>
+              <rectangle name="rect-1" height="0.2" width="0.4" color="white" thickness={4} background="black">
+                <stackPanel>
+                  <textBlock text="Your Turn" color="white" fontSize={50} />
+                  {['Move', 'Fire', 'Reload', 'Rotate Turret'].map((action) => (
+                    <button key={action} color="white" onClick={() => handleActionClick(action as Action)}>
+                      <textBlock text={action} color="white" fontSize={20} />
+                    </button>
+                  ))}
+                </stackPanel>
+              </rectangle>
+            </advancedDynamicTexture>
+          </plane>
+        
+        {/* rest of your scene */}
+      </Scene>
+    </Engine>
+  );
 }
 
-const PointerDragBehavior: FC = () => {
-  const { room } = useContext(ColyseusContext);
-  return (
-    <div style={{ flex: 1, display: 'flex' }}>
-      <Engine antialias adaptToDeviceRatio canvasId="babylon-js">
-        <Scene>
-          <freeCamera
-            name="camera1"
-            position={new Vector3(0, 5, -10)}
-            setTarget={[Vector3.Zero()]}
-          />
-          <hemisphericLight
-            name="light1"
-            intensity={0.7}
-            direction={Vector3.Up()}
-          />
-          <WithPointerDragBehavior />
-        </Scene>
-      </Engine>
-    </div>
-  );
-};
-
-export default PointerDragBehavior
+export default TonkRoom;
