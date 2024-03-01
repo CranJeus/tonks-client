272119b5b71f3e029c12204c44dedd7454c16fbd
Jordan Goyena-Segarra <goyenasegarrajordan@gmail.com>
2024-03-01 14:37:55 -0500

    Removed BabylonJS

--- src/pages/TonkRoom/TonkRoom.tsx
+++ src/pages/TonkRoom/TonkRoom.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { Engine, Scene, AdvancedDynamicTexture, Rectangle, TextBlock, StackPanel, Button } from 'react-babylonjs';
 import { ColyseusContext } from '../../components/ColyseusProvider';
 
 type Action = 'Move' | 'Fire' | 'Reload' | 'Rotate Turret';
@@ -31,28 +30,7 @@ function TonkRoom() {
 
   if(!room)return;
   
-  return (
-    <Engine antialias adaptToDeviceRatio canvasId='babylonJS'>
-      <Scene>
-        
-          <plane name="gui-plane" size={5}>
-            <advancedDynamicTexture name="adTexture" createForParentMesh hasAlpha>
-              <rectangle name="rect-1" height="0.2" width="0.4" color="white" thickness={4} background="black">
-                <stackPanel>
-                  <textBlock text="Your Turn" color="white" fontSize={50} />
-                  {['Move', 'Fire', 'Reload', 'Rotate Turret'].map((action) => (
-                    <button key={action} color="white" onClick={() => handleActionClick(action as Action)}>
-                      <textBlock text={action} color="white" fontSize={20} />
-                    </button>
-                  ))}
-                </stackPanel>
-              </rectangle>
-            </advancedDynamicTexture>
-          </plane>
-        
-        {/* rest of your scene */}
-      </Scene>
-    </Engine>
+  return (<div></div>
   );
 }
 
